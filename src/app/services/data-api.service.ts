de8da206617a8761b885f6d0385b97e41d01c88b
root <root@0000>
2019-09-23 12:27:31 -0400

    fake api address

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -27,88 +27,88 @@ export class DataApiService {
   		});
 
 	getAllTixs(){
-		const url_api = 'https://www.thetixsapp.com:3000/api/product?filter[where][status]=activated';
+		const url_api = 'https://www.thetixsapi.com:3000/api/product?filter[where][status]=activated';
 		return this.http.get(url_api);
 
 	}
 	getTixById(id:string){
 		//console.log(id);
 		let indice = id;
-		const url_api=`https://www.thetixsapp.com:3000/api/product/${indice}`;
+		const url_api=`https://www.thetixsapi.com:3000/api/product/${indice}`;
 		this.tix = this.http.get(url_api);
 		return (this.tix);
 	}
 	getCardById(id:string){
 		let indice = id;
-		const url_api=`https://www.thetixsapp.com:3000/api/card/${indice}`;
+		const url_api=`https://www.thetixsapi.com:3000/api/card/${indice}`;
 		this.card = this.http.get(url_api);
 		return (this.card);
 	}
 	getPending(){
-		const url_api='https://www.thetixsapp.com:3000/api/product?filter[where][status]=pending';
+		const url_api='https://www.thetixsapi.com:3000/api/product?filter[where][status]=pending';
 		return (this.tixs = this.http.get(url_api));
 	}
 	getUsersPending(){
-		const url_api='https://www.thetixsapp.com:3000/api/card?filter[where][status]=pending';
+		const url_api='https://www.thetixsapi.com:3000/api/card?filter[where][status]=pending';
 		return (this.cards = this.http.get(url_api));
 	}
 
 	getActivePartners(){
-		const url_api='https://www.thetixsapp.com:3000/api/card?filter[where][and][0][status]=active&filter[where][and][1][type]=partnerType';
+		const url_api='https://www.thetixsapi.com:3000/api/card?filter[where][and][0][status]=active&filter[where][and][1][type]=partnerType';
 		return (this.cards = this.http.get(url_api));
 	}
 	getPendingPartners(){
-		const url_api='https://www.thetixsapp.com:3000/api/card?filter[where][and][0][status]=pending&filter[where][and][1][type]=partnerType';
+		const url_api='https://www.thetixsapi.com:3000/api/card?filter[where][and][0][status]=pending&filter[where][and][1][type]=partnerType';
 		return (this.cards = this.http.get(url_api));
 	}
 
 	getActiveAffiliates(){
-		const url_api='https://www.thetixsapp.com:3000/api/card?filter[where][and][0][status]=active&filter[where][and][1][type]=affiliateType';
+		const url_api='https://www.thetixsapi.com:3000/api/card?filter[where][and][0][status]=active&filter[where][and][1][type]=affiliateType';
 		return (this.cards = this.http.get(url_api));
 	}
 	getPendingAffiliates(){
-		const url_api='https://www.thetixsapp.com:3000/api/card?filter[where][and][0][status]=pending&filter[where][and][1][type]=affiliateType';
+		const url_api='https://www.thetixsapi.com:3000/api/card?filter[where][and][0][status]=pending&filter[where][and][1][type]=affiliateType';
 		return (this.cards = this.http.get(url_api));
 	}
 
 
 	saveTix(tix :TixInterface){
 		let token = this.authService.getToken();
-		const url_api='https://www.thetixsapp.com:3000/api/product?access_token${token}';
+		const url_api='https://www.thetixsapi.com:3000/api/product?access_token${token}';
 		return this.http
 		.post<TixInterface>(url_api, tix,{headers: this.headers})
 		.pipe(map(data => data));
 	}
 	saveTixFree(tix :TixInterface){
 	//	let token = this.authService.getToken();
-		const url_api='https://www.thetixsapp.com:3000/api/product';
+		const url_api='https://www.thetixsapi.com:3000/api/product';
 		return this.http
 		.post<TixInterface>(url_api, tix,{headers: this.headers})
 		.pipe(map(data => data));
 	}
 	updateTix(tix: TixInterface){
 		let token = this.authService.getToken();
-		const url_api='https://www.thetixsapp.com:3000/api/product?access_token${token}';
+		const url_api='https://www.thetixsapi.com:3000/api/product?access_token${token}';
 		return this.http
 		.put<TixInterface>(url_api, tix,{headers: this.headers})
 		.pipe(map(data => data));
 	}
 	deleteTix(id: string){
 		let token = this.authService.getToken();
-		const url_api='https://www.thetixsapp.com:3000/api/product?access_token${token}';
+		const url_api='https://www.thetixsapi.com:3000/api/product?access_token${token}';
 		return this.http
 		.delete<TixInterface>(url_api, {headers: this.headers})
 		.pipe(map(data => data));
 	}
 	getCards(id:string){
 		let indice = id;
-		const url_api = "https://www.thetixsapp.com:3000/api/card?filter[where][userd]=a"+indice;
+		const url_api = "https://www.thetixsapi.com:3000/api/card?filter[where][userd]=a"+indice;
 		return this.http.get(url_api);
 	}
 	saveCard(card: CardInterface){
 		//const token = this.authService.getToken();
 		//console.log(token);
-		const url_api='https://www.thetixsapp.com:3000/api/card';
+		const url_api='https://www.thetixsapi.com:3000/api/card';
 		return this.http
 		.post<CardInterface>(url_api, card,{headers: this.headers})
 		.pipe(map(data => data));
